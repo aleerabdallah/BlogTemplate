6bc130ca3ef192e8dda8426d9b6c28838b69a9fc
aleerabdallah <aleerabdallah7@gmail.com>
2024-05-03 23:46:26 +0200

    Second commit

--- base/base/urls.py
+++ base/base/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
+
+
+
+def home(request):
+    return render(request, 'base.html')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name="home"),
 ]
